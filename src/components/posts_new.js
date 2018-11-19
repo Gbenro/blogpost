2e25ce0fb466dfa9fbf5f2b2b562acbb96f78a62
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-19 13:08:37 -0600

    created Fetch_post

---
+++ src/components/posts_new.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+
+class PostsNew extends Component {
+ state={
+
+ }
+
+  render() {
+    return (
+      <div>
+       <h3>Post new</h3> 
+      </div>
+    )
+  }
+}
+
+
+
+export default connect()(PostsNew)
