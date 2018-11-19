bb2617c363599996a2e183b1db41080fe035e3aa
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-19 13:29:21 -0600

    added redux form

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
+import {Link} from 'react-router-dom';
+
 
  class PostIndex extends Component {
      componentDidMount(){
@@ -21,6 +23,11 @@ import {fetchPosts} from '../actions';
 
     return (
       <div>
+          <div className="text-xs-right">
+              <Link className = "btn btn-primary" to ="/posts/new">
+                Add a Post
+              </Link>
+          </div>
        <h3> Posts</h3>
        <ul className="list-group">
            {this.renderPosts()}
