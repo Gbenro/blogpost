c1269d8bacac5093c4d1dd8f84281a435d8c2e42
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-19 14:00:55 -0600

    added input form for new post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Field, reduxForm} from 'redux-form';;
 
-import { connect } from 'react-redux'
 
 class PostsNew extends Component {
  state={
 
  }
+    renderField=(field)=>{
+        return (
+            <div className = "form-group">
+                <label>{field.label}</label>
+                <input className= "form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
 
-  render() {
-    return (
-      <div>
-       <h3>Post new</h3> 
-      </div>
-    )
-  }
-}
+  
+    render() {
+        return (
+        <form>
+            <Field 
+            name= "title"
+            label="Title"
+            component={this.renderField} 
+            />
+            <Field
+            name='tags'
+            label="Tags"
+            component = {this.renderField}
+            />
+            <Field
+            name='content'
+            label="Post Content"
+            component = {this.renderField}
+            />
+        </form>
+        );
+    }
+    }
 
 
 
-export default connect()(PostsNew)
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
+
+
