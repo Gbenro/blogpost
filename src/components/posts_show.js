abb40c9456c75aeb96e57b265f33a68163141495
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-20 13:41:46 -0600

    having some errors

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {fetchPost} from '../actions';
+import {Link} from 'react-redux';
 
 class PostsShow extends Component {
     componentDidMount(){
       const{id}=  this.props.match.params;
-        this.props.fetchPost();
+        this.props.fetchPost(id);
     }
   render() {
+
+    const {post} = this.props;
+    if(!post){
+      return(
+        <div>Loading.....</div>
+      )
+    }
+
     return (
       <div>
-        posts Show!!
+        <Link to ="/" className= "btn btn-primary">Back to Index</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
-  };
+  }
 }
 
 function mapStateToProps({posts}, ownProps) {
-return {posts:posts[ownProps.match.params.id]};
+return {post:posts[ownProps.match.params.id]};
 }
 
-export default connect(null,{fetchPost})(PostsShow);
+export default connect(mapStateToProps,{fetchPost})(PostsShow);
